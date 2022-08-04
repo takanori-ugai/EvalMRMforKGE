1dd6755b2fe877149b3aef3bb5a89645685264c0
Takanori Ugai <ugai@fujitsu.com>
2022-08-04 09:28:31 +0900

    reformat

--- train_analogy_SPO.py
+++ train_analogy_SPO.py
@@ -7,14 +7,14 @@ from openke.data import TrainDataLoader, TestDataLoader
 
 # dataloader for training
 train_dataloader = TrainDataLoader(
-	in_path = "./benchmarks/SPO/", 
-	nbatches = 100,
-	threads = 8, 
-	sampling_mode = "normal", 
-	bern_flag = 1, 
-	filter_flag = 1, 
-	neg_ent = 25,
-	neg_rel = 0
+    in_path="./benchmarks/SPO/",
+    nbatches=100,
+    threads=8,
+    sampling_mode="normal",
+    bern_flag=1,
+    filter_flag=1,
+    neg_ent=25,
+    neg_rel=0
 )
 
 # dataloader for test
@@ -22,25 +22,26 @@ test_dataloader = TestDataLoader("./benchmarks/SPO/", "link")
 
 # define the model
 analogy = Analogy(
-	ent_tot = train_dataloader.get_ent_tot(),
-	rel_tot = train_dataloader.get_rel_tot(),
-	dim = 200
+    ent_tot=train_dataloader.get_ent_tot(),
+    rel_tot=train_dataloader.get_rel_tot(),
+    dim=200
 )
 
 # define the loss function
 model = NegativeSampling(
-	model = analogy, 
-	loss = SoftplusLoss(),
-	batch_size = train_dataloader.get_batch_size(), 
-	regul_rate = 1.0
+    model=analogy,
+    loss=SoftplusLoss(),
+    batch_size=train_dataloader.get_batch_size(),
+    regul_rate=1.0
 )
 
 # train the model
-trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 2000, alpha = 0.5, use_gpu = True, opt_method = "adagrad")
+trainer = Trainer(model=model, data_loader=train_dataloader, train_times=2000, alpha=0.5, use_gpu=True,
+                  opt_method="adagrad")
 trainer.run()
 analogy.save_checkpoint('./checkpoint/spo.ckpt')
 
 # test the model
 analogy.load_checkpoint('./checkpoint/spo.ckpt')
-tester = Tester(model = analogy, data_loader = test_dataloader, use_gpu = True)
-tester.run_link_prediction(type_constrain = False)
+tester = Tester(model=analogy, data_loader=test_dataloader, use_gpu=True)
+tester.run_link_prediction(type_constrain=False)
--- train_transd_SPO_isA.py
+++ train_transd_SPO_isA.py
@@ -7,41 +7,40 @@ from openke.data import TrainDataLoader, TestDataLoader
 
 # dataloader for training
 train_dataloader = TrainDataLoader(
-	in_path = "./benchmarks/SPO-isA/", 
-	nbatches = 100,
-	threads = 8, 
-	sampling_mode = "normal", 
-	bern_flag = 1, 
-	filter_flag = 1, 
-	neg_ent = 25,
-	neg_rel = 0)
+    in_path="./benchmarks/SPO-isA/",
+    nbatches=100,
+    threads=8,
+    sampling_mode="normal",
+    bern_flag=1,
+    filter_flag=1,
+    neg_ent=25,
+    neg_rel=0)
 
 # dataloader for test
 test_dataloader = TestDataLoader("./benchmarks/SPO-isA/", "link")
 
 # define the model
 transd = TransD(
-	ent_tot = train_dataloader.get_ent_tot(),
-	rel_tot = train_dataloader.get_rel_tot(),
-	dim_e = 200, 
-	dim_r = 200, 
-	p_norm = 1, 
-	norm_flag = True)
-
+    ent_tot=train_dataloader.get_ent_tot(),
+    rel_tot=train_dataloader.get_rel_tot(),
+    dim_e=200,
+    dim_r=200,
+    p_norm=1,
+    norm_flag=True)
 
 # define the loss function
 model = NegativeSampling(
-	model = transd, 
-	loss = MarginLoss(margin = 4.0),
-	batch_size = train_dataloader.get_batch_size()
+    model=transd,
+    loss=MarginLoss(margin=4.0),
+    batch_size=train_dataloader.get_batch_size()
 )
 
 # train the model
-trainer = Trainer(model = model, data_loader = train_dataloader, train_times = 1000, alpha = 1.0, use_gpu = False)
+trainer = Trainer(model=model, data_loader=train_dataloader, train_times=1000, alpha=1.0, use_gpu=False)
 trainer.run()
 transd.save_checkpoint('./checkpoint/transd_spo_isA.ckpt')
 
 # test the model
 transd.load_checkpoint('./checkpoint/transd_spo_isA.ckpt')
-tester = Tester(model = transd, data_loader = test_dataloader, use_gpu = False)
-tester.run_link_prediction(type_constrain = True)
+tester = Tester(model=transd, data_loader=test_dataloader, use_gpu=False)
+tester.run_link_prediction(type_constrain=True)
