b4ebb451682df19d7d09005429a8a0437ba47035
Takanori Ugai <ugai@jp.fujitsu.com>
2022-11-16 01:47:55 +0900

    add a script

---
+++ afo.sh
@@ -0,0 +1,7 @@
+python train_transe_4S.py >& TransE.log 
+python train_complEx_4S.py >& ComplEx.log
+python train_rotate_4S.py >& RotaTe.log
+python train_transd_4S.py >& TransD.log
+python train_transh_4S.py >& TransH.log
+python train_analogy_4S.py >& Analogy.log
+python train_rescal_4S.py >& Rescal.log
